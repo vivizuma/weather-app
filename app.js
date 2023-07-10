4962c9d2cf3268520e4172b11e8e6440dd0df45e
Vito <vito.manzullo@live.co.uk>
2023-07-10 09:14:58 +0100

    api key

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const APP = {
     console.log("script loaded");
   },
   
+  
 };
 
 document.addEventListener("DOMContentLoaded", APP.init);
+fetch("http://api.weatherapi.com/v1/forecast.json?key=146bc6e955bc4674a65135729230707&q=London&days=1&aqi=no&alerts=no")
+.then(function(response){
+  return response.json();
+})
+.then(function(response){
+  console.log(response)
+})
