a6634d4a8ec2b58139350190f21cc130b3d61416
Vito <vitomanz@hotmail.co.uk>
2023-07-11 16:52:39 +0100

    ...

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
+let jsonData;
+
+async function fetchData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const json = await response.json();
+  jsonData = json;
+  console.log(jsonData);
+}
+
+fetchData();
+
 const APP = {
   init() {
     console.log("script loaded");
+    APP.eventListeners();
   },
-  eventListeners() {},
+  eventListeners() {
+    const searchBtn = document.getElementById("search-btn");
+    searchBtn.addEventListener("click", APP.searchClicked);
+  },
+  searchClicked() {
+    console.log("hello");
+    const input = document.getElementById("search-field");
+    const value = input.value;
+    console.log(input.value);
+    getWeather(value);
+  },
+  getWeather(location) {
+    fetch(
+      "http://api.weatherapi.com/v1/forecast.json?key=146bc6e955bc4674a65135729230707&q=Atlanta&days=1&aqi=no&alerts=no"
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        console.log(response);
+      });
+  },
 };
-
-document.addEventListener("DOMContentLoaded", APP.init);
-function getWeather(location) {
-  fetch(
-    "http://api.weatherapi.com/v1/forecast.json?key=146bc6e955bc4674a65135729230707&q=Atlanta&days=1&aqi=no&alerts=no"
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      console.log(response);
-    });
-}
 
 const apiKey = "146bc6e955bc4674a65135729230707";
 const apiUrl = "http://api.weatherapi.com/v1/forecast.json?key=";
@@ -26,7 +46,19 @@ const baseUrl = "http://api.weatherapi.com/v1";
 // forecast
 // + api key
 // + user input search value
-// 
+//
 // "http://api.weatherapi.com/v1/forecast.json?key=146bc6e955bc4674a65135729230707&q=Atlanta&days=1&aqi=no&alerts=no"
-// 
-// 
+//
+//
+
+// async function fetchWeather(location, apiKey, baseUrl) {
+//   fetch(
+//     `${baseUrl}/forecast.json?key=${apiKey}&q=${location}&days=1&aqi=no&alerts=no`
+//   )
+//     .then((res) => res.json())
+//     .then((data) => console.log(data));
+// }
+
+// fetchWeather("London", apiKey, baseUrl);
+
+document.addEventListener("DOMContentLoaded", APP.init);
